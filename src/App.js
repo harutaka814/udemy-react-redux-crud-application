53839d244a091d92be6f10b3cab6f174a1ff1076
harutaka814 <harutaka814@gmail.com>
2020-06-23 17:13:07 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <h1>Hello World!!</h1>
-  );
+// function App() {
+//   return (
+//     <div>Hello World!!</div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    // const greeting = "Hi, Tom!";
+    // const dom = <h1 className="foo">{greeting}</h1>; 
+    // return dom;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => console.log("I am clicked.")}></input>
+      </React.Fragment>
+    )
+    // return <input type="text" onChange={() => console.log("I am clicked.")}></input>
+  }
 }
-
 
 // function App() {
 //   return React.createElement(
