47262a08280bb93c06c84c2fbd346ae40846a7dd
harutaka814 <harutaka814@gmail.com>
2020-06-24 17:32:54 +0900

    propsについて学習

--- src/App.js
+++ src/App.js
@@ -22,11 +22,29 @@ import React from 'react';
 // }
 
 const App = () => {
-  return <div><Cat></Cat><Cat></Cat><Cat></Cat></div>
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName" }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+      {/* <User name={"Taro"} age={10}/>
+      <User name={"Hanako"} age={5}/> */}
+    </div>)
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, T am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 // function App() {
 //   return React.createElement(
